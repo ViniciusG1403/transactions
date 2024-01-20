346d8b6934cdbdcb3ea2b1b931d207665d2c29b3
Vinicius <vinicius.prado@nexuscloud.com.br>
2024-01-19 23:08:47 -0300

    Adiciona entidade de usuario

--- src/main/java/entities/Usuario.java
+++ src/main/java/entities/Usuario.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 /**
  * @author Vinicius Gabriel <vinicius.prado@nexuscloud.com.br>
@@ -11,11 +15,28 @@ import jakarta.persistence.Table;
  * @since 19/01/24
  */
 @Entity
+@Getter
+@Setter
 @Table(name = "usuario")
 public class Usuario {
 
     @Id
     @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "saldo", nullable = false)
+    private BigDecimal saldo;
 
 }
